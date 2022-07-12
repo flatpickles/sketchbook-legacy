aa58ead1a66375affb5f4ec35184888b63ebe780
Matt Nichols <mattnichols206@gmail.com>
2022-07-11 23:13:17 -0700

    Cleaning up the template a bit

--- src/CanvasSketchEditor.svelte
+++ src/CanvasSketchEditor.svelte
@@ -1,18 +1,18 @@
 <script>
-	import CanvasSketch from './CanvasSketch.svelte';
-	export let settings = {};
-	export let data = {};
-	export let sketch = () => {};
-	
-	let localStorageSupported = (() => {
-		try {
-			return typeof window.localStorage !== 'undefined';
-		} catch (err) {
-			return false;
-		}
-	})();
+    import CanvasSketch from './CanvasSketch.svelte';
+    export let settings = {};
+    export let data = {};
+    export let sketch = () => {};
+    
+    let localStorageSupported = (() => {
+        try {
+            return typeof window.localStorage !== 'undefined';
+        } catch (err) {
+            return false;
+        }
+    })();
 
-	// None of this will work in the sandbox REPL but it will work offline
+    // None of this will work in the sandbox REPL but it will work offline
   readData(settings, data);
   $: saveData(settings, data);
 
@@ -37,49 +37,49 @@
 </script>
 
 <main>
-	<div class='viewport'>
-		<CanvasSketch {data} {settings} {sketch} />
-	</div>
-	<div class='panel'>
-		<slot></slot>
-	</div>
+    <div class='viewport'>
+        <CanvasSketch {data} {settings} {sketch} />
+    </div>
+    <div class='panel'>
+        <slot></slot>
+    </div>
 </main>
 
 <style>
-	:global(body) {
-		margin: 0;
-		padding: 0;
-	}
-	
-	main {
-		width: 100%;
-		height: 100%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: row;
-	}
-	.viewport {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		height: 100%;
-		flex-basis: 60%;
-		min-width: 200px;
-		flex-grow: 1;
-		flex-shrink: 1;
-	}
-	.panel {
-		padding: 20px;
-		box-sizing: border-box;
-		flex-basis: 300px;
-		min-width: 200px;
-		max-width: 400px;
-		flex-grow: 1;
-		flex-shrink: 1;
-		height: 100%;
-		background: hsl(0, 0%, 95%);
-		border-left: 1px solid hsl(0, 0%, 90%);
-	}
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
+    
+    main {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+    }
+    .viewport {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 100%;
+        flex-basis: 60%;
+        min-width: 200px;
+        flex-grow: 1;
+        flex-shrink: 1;
+    }
+    .panel {
+        padding: 20px;
+        box-sizing: border-box;
+        flex-basis: 300px;
+        min-width: 200px;
+        max-width: 400px;
+        flex-grow: 1;
+        flex-shrink: 1;
+        height: 100%;
+        background: hsl(0, 0%, 95%);
+        border-left: 1px solid hsl(0, 0%, 90%);
+    }
 </style>

4dcef700c00d7136a7c293e77048e21581790045
Matt Nichols <mattnichols206@gmail.com>
2022-09-23 10:54:37 -0700

    [Floppy Disk] Params

--- src/Sketches/FloppyDisk.js
+++ src/Sketches/FloppyDisk.js
@@ -1,15 +1,15 @@
 /** Imports assume file is contained within Sketches **/
 
 import Sketch, { SketchType } from './Base/Sketch.js';
-import { FloatParam, BoolParam } from './Base/SketchParam.js';
+import { FloatParam, ColorParam } from './Base/SketchParam.js';
 import createShader from  'canvas-sketch-util/shader';
 
 import shaderString from './FloppyDisk.frag';
 
 export default class FloppyDisk extends Sketch {
-    name = "Floppy Disk";
+    name = 'Floppy Disk';
     type = SketchType.Shader;
-    // date = new Date("9/23/23");
+    // date = new Date('9/23/23');
 
     settings = {
         context: 'webgl',
@@ -18,7 +18,17 @@ export default class FloppyDisk extends Sketch {
     };
 
     params = {
-        blueness: new FloatParam("Blueness", 0.0, 0.0, 1.0)
+        innerSize: new FloatParam('Inner Size', 0.3, 0.0, 1.0),
+        outerSize: new FloatParam('Outer Size', 0.8, 0.0, 1.0),
+        wobbleShape1: new FloatParam('Shape 1', 5.0, 3.0, 10.0),
+        wobbleFactor1: new FloatParam('Factor 1', 0.02, 0.0, 0.2),
+        wobbleShape2: new FloatParam('Shape 2', 5.0, 3.0, 10.0),
+        wobbleFactor2: new FloatParam('Factor 2', 0.02, 0.0, 0.2),
+        centerColor: new ColorParam('Center Color', '#431F0E'),
+        color1: new ColorParam('Color 1', '#CC8154'),
+        color2: new ColorParam('Color 2', '#96542E'),
+        color3: new ColorParam('Color 3', '#622E0F'),
+        backgroundColor: new ColorParam('BG Color', '#431F0E'),
     };
 
     sketchFn = ({ gl }) => {
@@ -28,7 +38,7 @@ export default class FloppyDisk extends Sketch {
             frag,
             uniforms: {
                 time: ({ time }) => time,
-                blueness: ({}) => this.params.blueness.value
+                blueness: ({}) => this.params.innerSize.value
             }
         });
     };

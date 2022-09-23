985d83aa642890dd596f3d27289a828bcbf04f76
Matt Nichols <mattnichols206@gmail.com>
2022-09-23 11:36:11 -0700

    Remove demos directory

--- src/SketchIndex.js
+++ src/SketchIndex.js
@@ -1,14 +1,8 @@
-import SketchCanvasDemo from './Sketches/Demos/SketchCanvasDemo.js';
-import SketchShaderDemo from './Sketches/Demos/SketchShaderDemo.js';
-import SketchCanvasDemo2 from './Sketches/Demos/SketchCanvasDemo2.js';
 import NoSignal from './Sketches/NoSignal.js';
 import Rectilinear from './Sketches/Rectilinear.js';
 import FloppyDisk from './Sketches/FloppyDisk.js';
 
 const sketches = [
-    // new SketchCanvasDemo(),
-    // new SketchShaderDemo(),
-    // new SketchCanvasDemo2(),
     new Rectilinear(),
     new NoSignal(),
     new FloppyDisk(),

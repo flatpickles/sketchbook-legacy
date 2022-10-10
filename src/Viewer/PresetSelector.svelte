553181a64d2269a11ea9885ccf0b21fb1a509d31
Matt Nichols <mattnichols206@gmail.com>
2022-10-09 22:36:08 -0700

    Beginnings of preset actions menu

--- src/Viewer/PresetSelector.svelte
+++ src/Viewer/PresetSelector.svelte
@@ -18,10 +18,25 @@
     export function paramsUpdated() {
         modifiedText = sketch.presetModified ? ' *' : '';
     }
+
+    // Show & hide the preset actions menu
+    let menuVisible = false;
+    function toggleMenu() {
+        menuVisible = !menuVisible;
+    }
+
+    /*
+
+    * Hide menu when choosing new preset, switching sketch
+    * Increase tap target size
+    * Make the menu actions do things!
+    * Hide/show menu actions when relevant
+
+    */
 </script>
 
-<div id='preset_selector'>
-    <div id='select_container'>
+<div class='preset_selector'>
+    <div class='select_container'>
         <select bind:this={selectElement} on:change={presetSelected}>
             {#each Object.keys(sketch.availablePresets) as presetName}
                 {#if presetName === sketch.selectedPresetName}
@@ -32,13 +47,30 @@
             {/each}
         </select>
     </div>
-    <div id='new_button'>
-        +
+    <div class='menu'>
+        <div class='menu_button' on:click={toggleMenu}>&ctdot;</div>
+        <div class='menu_content' class:open={menuVisible}>
+            <div class='menu_item'>
+                Reset
+            </div>
+            <div class='menu_item'>
+                New
+            </div>
+            <div class='menu_item'>
+                Remove
+            </div>
+            <div class='menu_item'>
+                Import
+            </div>
+            <div class='menu_item'>
+                Export
+            </div>
+        </div>
     </div>
 </div>
 
 <style>
-    #preset_selector {
+    .preset_selector {
         border-top: var(--border);
         padding: 0;
         font-size: var(--description-font-size);
@@ -46,7 +78,7 @@
         align-items: baseline;
     }
 
-    #select_container {
+    .select_container {
         flex-grow: 1;
         flex-shrink: 0;
     }
@@ -66,9 +98,45 @@
         flex-grow: 0;
     }
 
-    #new_button {
+    .menu {
+        position: relative;
         padding: var(--spacing);
+        display: inline-block;
+    }
+
+    .menu_button {
         text-align: right;
-        
+        cursor: pointer;
+    }
+
+    .menu_content {
+        display: none;
+        flex-direction: column;
+        position: absolute;
+        z-index: 1;
+        right: 0;
+        border: var(--border);
+        font-size: var(--description-font-size);
+        color: #000;
+        background-color: var(--panel-background);
+        backdrop-filter: var(--panel-filter);
+        -webkit-backdrop-filter: var(--panel-filter);
+        border-radius: 2px;
+    }
+
+    .menu .open {
+        display: flex;
+    }
+
+    .menu_item {
+        flex-grow: 1;
+        padding: 2px;
+        padding-left: var(--spacing);
+        padding-right: var(--spacing);
+        cursor: pointer;
+    }
+
+    .menu_item:hover {
+        background-color: var(--selected-bg-color);
     }
 </style>

059af5ef3455109f1d9f5b4e75703b27c1f4c38d
Matt Nichols <mattnichols206@gmail.com>
2022-12-10 16:32:42 -0800

    [Discloser] Reposition triangle for centered rotation

--- src/Viewer/Components/Discloser.svelte
+++ src/Viewer/Components/Discloser.svelte
@@ -1,6 +1,6 @@
 <script type="ts">
     export let open: boolean;
-    const size = 10;
+    const size = 12;
 </script>
 
 <div style="--size: {size}" class="triangle" class:rotated={!open} />
@@ -10,7 +10,7 @@
         width: calc(var(--size) * 1px);
         height: calc(var(--size) * 1px);
         background-color: black;
-        clip-path: polygon(0 6.7%, 100% 6.7%, 50% 100%);
+        clip-path: polygon(6.7% 25%, 93.3% 25%, 50% 100%);
         transition: transform var(--disclosure-animation-time) ease-out;
         transform: translate(0%, -25%) rotate(0deg);
         -webkit-transform: translate(0%, -25%) rotate(0deg);

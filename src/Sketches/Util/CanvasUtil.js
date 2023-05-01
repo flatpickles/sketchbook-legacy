3a8cf6484566a0922f6b6bf3ff49fd3f67304ba3
Matt Nichols <mattnichols206@gmail.com>
2023-04-30 19:33:33 -0700

    Bezier debug visuals for splines

--- src/Sketches/Util/CanvasUtil.js
+++ src/Sketches/Util/CanvasUtil.js
@@ -1,20 +1,75 @@
 import BezierSpline from 'bezier-spline';
 
+const DEBUG = true;
+
 export default class CanvasUtil {
     static drawSpline(context, points, strokeWeight = 1, strokeStyle = '#000') {
         if (points.length < 2) { throw 'Spline can only be drawn with two or more points.'; }
         const closed = points[0].x === points[points.length - 1].x && points[0].y === points[points.length - 1].y;
         // todo - implement closed splines
     
+        // Create the spline object and begin a path at the first knot
         const spline = new BezierSpline(points);
         const splinePath = new Path2D();
         splinePath.moveTo(spline.knots[0][0], spline.knots[0][1]);
+
+        // Iterate through curves to each next knot
         for (let curveIdx = 0; curveIdx < spline.curves.length; curveIdx++) {
             const curve = spline.curves[curveIdx];
             splinePath.bezierCurveTo(curve[1][0], curve[1][1], curve[2][0], curve[2][1], curve[3][0], curve[3][1]);
+            
+            // Draw the control points (in debug mode)
+            if (DEBUG) {
+                context.save();
+                context.lineWidth = 2;
+                const indicatorSize = 4;
+
+                // Control Line 1
+                context.strokeStyle = '#f00';
+                context.beginPath();
+                context.moveTo(curve[0][0], curve[0][1]);
+                context.lineTo(curve[1][0], curve[1][1]);
+                context.stroke();
+
+                // Control Point 1
+                context.fillStyle = '#f00';
+                context.beginPath();
+                context.arc(curve[1][0], curve[1][1], indicatorSize, 0, 2 * Math.PI);
+                context.fill();
+
+                // Control Line 2
+                context.strokeStyle = '#0f0';
+                context.beginPath();
+                context.moveTo(curve[3][0], curve[3][1]);
+                context.lineTo(curve[2][0], curve[2][1]);
+                context.stroke();
+
+                // Control Point 2
+                context.fillStyle = '#0f0';
+                context.beginPath();
+                context.arc(curve[2][0], curve[2][1], indicatorSize, 0, 2 * Math.PI);
+                context.fill();
+
+                context.restore();
+            }
         }
-        console.log(spline);
+
+        // Draw the knot points (in debug mode)
+        if (DEBUG) {
+            const indicatorSize = 5;
+            spline.knots.forEach((knot) => {
+                context.save();
+                context.fillStyle = '#000';
+                context.beginPath();
+                context.arc(knot[0], knot[1], indicatorSize, 0, 2 * Math.PI);
+                context.fill();
+                context.restore();
+            });
+        }
+
+        // Close (if relevant) and draw
         if (closed) splinePath.closePath();
+        context.strokeStyle = strokeStyle;
         context.stroke(splinePath);
     }
 

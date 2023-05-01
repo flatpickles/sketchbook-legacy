bf5df064911663325cec4acb4ce6967c0320580a
Matt Nichols <mattnichols206@gmail.com>
2023-05-01 12:48:55 -0700

    Convert CanvasUtil to TypeScript

--- src/Sketches/Util/CanvasUtil.ts
+++ src/Sketches/Util/CanvasUtil.ts
@@ -1,28 +1,38 @@
-import BezierSpline from 'bezier-spline';
-
-const DEBUG = true;
+import BezierSpline from "bezier-spline";
 
 export default class CanvasUtil {
-    static drawSpline(context, points, strokeWeight = 1, strokeStyle = '#000') {
-        if (points.length < 3) { throw 'Spline can only be drawn with three or more points.'; }
-        
+    static drawSpline(
+        context: CanvasRenderingContext2D,
+        points: [number, number][],
+        strokeWeight = 1,
+        strokeStyle = "#000",
+        debug = false
+    ) {
+        if (points.length < 3) {
+            throw "Spline can only be drawn with three or more points.";
+        }
+
         // If the spline is closed, expand with points from the opposite end
         const leadingSize = 3; // number of adjacent points for path close smoothing
-        const closed = leadingSize > 0 && 
-                       points[0][0] == points[points.length - 1][0] &&
-                       points[0][1] == points[points.length - 1][1];
-        if (closed && points.length <= 3) { throw 'Spline can only be closed with four or more points (including duplicated endpoint).'}
+        const closed =
+            leadingSize > 0 &&
+            points[0][0] == points[points.length - 1][0] &&
+            points[0][1] == points[points.length - 1][1];
+        if (closed && points.length <= 3) {
+            throw "Spline can only be closed with four or more points (including duplicated endpoints).";
+        }
         if (closed) {
             // Collect leading and trailing knots, accommodating wrap-around for small paths
             let trailingIndex = 1;
             let leadingIndex = points.length - 2;
-            let leadingKnots = [];
-            let trailingKnots = [];
+            let leadingKnots: [number, number][] = [];
+            let trailingKnots: [number, number][] = [];
             for (let lead = 0; lead < leadingSize; lead++) {
                 trailingKnots.push(points[trailingIndex]);
                 leadingKnots.unshift(points[leadingIndex]);
                 trailingIndex = (trailingIndex + 1) % points.length;
-                leadingIndex = (leadingIndex > 0) ? (leadingIndex - 1) : (points.length - 1) ;
+                leadingIndex =
+                    leadingIndex > 0 ? leadingIndex - 1 : points.length - 1;
             }
             points = [...leadingKnots, ...points, ...trailingKnots];
         }
@@ -41,38 +51,57 @@ export default class CanvasUtil {
         // Iterate through curves to each next knot
         for (let curveIdx = 0; curveIdx < spline.curves.length; curveIdx++) {
             const curve = spline.curves[curveIdx];
-            splinePath.bezierCurveTo(curve[1][0], curve[1][1], curve[2][0], curve[2][1], curve[3][0], curve[3][1]);
-            
+            splinePath.bezierCurveTo(
+                curve[1][0],
+                curve[1][1],
+                curve[2][0],
+                curve[2][1],
+                curve[3][0],
+                curve[3][1]
+            );
+
             // Draw the control points (in debug mode)
-            if (DEBUG) {
+            if (debug) {
                 context.save();
                 context.lineWidth = 2;
                 const indicatorSize = 4;
 
                 // Control Line 1
-                context.strokeStyle = '#f00';
+                context.strokeStyle = "#f00";
                 context.beginPath();
                 context.moveTo(curve[0][0], curve[0][1]);
                 context.lineTo(curve[1][0], curve[1][1]);
                 context.stroke();
 
                 // Control Point 1
-                context.fillStyle = '#f00';
+                context.fillStyle = "#f00";
                 context.beginPath();
-                context.arc(curve[1][0], curve[1][1], indicatorSize, 0, 2 * Math.PI);
+                context.arc(
+                    curve[1][0],
+                    curve[1][1],
+                    indicatorSize,
+                    0,
+                    2 * Math.PI
+                );
                 context.fill();
 
                 // Control Line 2
-                context.strokeStyle = '#0f0';
+                context.strokeStyle = "#0f0";
                 context.beginPath();
                 context.moveTo(curve[3][0], curve[3][1]);
                 context.lineTo(curve[2][0], curve[2][1]);
                 context.stroke();
 
                 // Control Point 2
-                context.fillStyle = '#0f0';
+                context.fillStyle = "#0f0";
                 context.beginPath();
-                context.arc(curve[2][0], curve[2][1], indicatorSize, 0, 2 * Math.PI);
+                context.arc(
+                    curve[2][0],
+                    curve[2][1],
+                    indicatorSize,
+                    0,
+                    2 * Math.PI
+                );
                 context.fill();
 
                 context.restore();
@@ -80,11 +109,11 @@ export default class CanvasUtil {
         }
 
         // Draw the knot points (in debug mode)
-        if (DEBUG) {
+        if (debug) {
             const indicatorSize = 5;
-            spline.knots.forEach((knot) => {
+            spline.knots.forEach((knot: [number, number]) => {
                 context.save();
-                context.fillStyle = '#000';
+                context.fillStyle = "#000";
                 context.beginPath();
                 context.arc(knot[0], knot[1], indicatorSize, 0, 2 * Math.PI);
                 context.fill();
@@ -98,7 +127,13 @@ export default class CanvasUtil {
         context.stroke(splinePath);
     }
 
-    static drawLine(context, pointA, pointB, strokeWeight = 1, strokeStyle = '#000') {
+    static drawLine(
+        context: CanvasRenderingContext2D,
+        pointA: { x: number; y: number },
+        pointB: { x: number; y: number },
+        strokeWeight = 1,
+        strokeStyle = "#000"
+    ) {
         if (strokeWeight <= 0) return;
         const linePath = new Path2D();
         linePath.moveTo(pointA.x, pointA.y);
@@ -108,8 +143,16 @@ export default class CanvasUtil {
         context.stroke(linePath);
     }
 
-    static drawShape(context, points, fillStyle = '#fff', strokeWeight = 0, strokeStyle = '#000') {
-        if (points.length < 3) { throw 'Shape can only be drawn with three or more points.'; }
+    static drawShape(
+        context: CanvasRenderingContext2D,
+        points: { x: number; y: number }[],
+        fillStyle = "#fff",
+        strokeWeight = 0,
+        strokeStyle = "#000"
+    ) {
+        if (points.length < 3) {
+            throw "Shape can only be drawn with three or more points.";
+        }
 
         // Create a path through the input vertices
         let shapeRegion = new Path2D();

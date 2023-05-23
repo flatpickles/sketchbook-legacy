fe76d5a0f28bb44147f2bee7772ee9092eba516d
Matt Nichols <mattnichols206@gmail.com>
2023-05-23 14:27:25 -0700

    Relax: better math for parameterization

--- src/Sketches/Projects/Relax/RelaxGenerator.ts
+++ src/Sketches/Projects/Relax/RelaxGenerator.ts
@@ -1,29 +1,46 @@
 import CurveUtil from '../../Util/PathUtil';
 import type { Path } from 'd3-path';
 
+const pointsPerSegment = 10; // two segments per polygon side
+const polygonSides = 4;
+const generationPercentage = 0.25;
+const rotationPercentage = 0.75;
+const polygonRotationPercentage = -0.07;
+const inset = 0.1;
+
 export default class RelaxGenerator {
-    public generate(size: [number, number], polygonSides = 4, pointsPerSide = 8): Path[] {
-        const resolution = polygonSides * pointsPerSide * 2 + 1;
-        const center = size.map((d) => d / 2);
+    public generate(size: [number, number]): Path[] {
+        // Constants for positioning and sizing
+        const topLeft = size.map((d) => inset * d);
+        const bottomRight = size.map((d) => d * (1 - inset));
+        const fullResolution = polygonSides * pointsPerSegment * 2 + 1;
+        const center = [topLeft[0], bottomRight[1]];
         const radius = Math.min(size[0], size[1]) / 4;
 
-        const rotation = ((Math.PI * 2) / (resolution - 1)) * 4; // sharp corners
+        // Adjustments computed in point offsets to maintain sharp corners
+        const pointsToGenerate = Math.ceil(fullResolution * generationPercentage);
+        const pointRotationOffset = Math.ceil(rotationPercentage * (fullResolution - 1));
+        const polygonPointRotationOffset = Math.ceil(
+            polygonRotationPercentage * (fullResolution - 1)
+        );
+
+        // Calculate points around a circle
         const circlePoints: [number, number][] = [];
-        for (let pointIdx = 0; pointIdx < resolution / 3; pointIdx++) {
-            const angle = (pointIdx / (resolution - 1)) * 2 * Math.PI;
+        for (let pointIdx = 0; pointIdx < pointsToGenerate; pointIdx++) {
+            const angle = ((pointIdx + pointRotationOffset) / (fullResolution - 1)) * 2 * Math.PI;
             const point: [number, number] = [
-                center[0] + radius * Math.cos(angle + rotation),
-                center[1] + radius * Math.sin(angle + rotation),
+                center[0] + radius * Math.cos(angle),
+                center[1] + radius * Math.sin(angle),
             ];
             circlePoints.push(point);
         }
 
-        const polygonRotationOffset = ((Math.PI * 2) / (resolution - 1)) * 2;
+        // Calculate points around a polygon
         const polygonPoints: [number, number][] = [];
-        for (let pointIdx = 0; pointIdx < resolution / 3; pointIdx++) {
+        for (let pointIdx = 0; pointIdx < pointsToGenerate; pointIdx++) {
             // reference: https://www.youtube.com/watch?v=AoOv6bWg9lk
             const theta =
-                (pointIdx / (resolution - 1)) * 2 * Math.PI + rotation - polygonRotationOffset;
+                ((pointIdx - polygonPointRotationOffset) / (fullResolution - 1)) * 2 * Math.PI;
             const polyR =
                 (radius * Math.cos(Math.PI / polygonSides)) /
                 Math.cos(
@@ -32,13 +49,18 @@ export default class RelaxGenerator {
                             Math.floor((polygonSides * theta + Math.PI) / (2 * Math.PI))
                 );
 
+            const thetaCorrection =
+                ((pointRotationOffset + polygonPointRotationOffset) / (fullResolution - 1)) *
+                2 *
+                Math.PI;
             const point: [number, number] = [
-                center[0] + polyR * Math.cos(theta + polygonRotationOffset),
-                center[1] + polyR * Math.sin(theta + polygonRotationOffset),
+                center[0] + polyR * Math.cos(theta + thetaCorrection),
+                center[1] + polyR * Math.sin(theta + thetaCorrection),
             ];
             polygonPoints.push(point);
         }
 
+        // Return the two paths
         return [
             CurveUtil.createCardinalSpline(circlePoints, 1),
             CurveUtil.createCardinalSpline(polygonPoints, 0),

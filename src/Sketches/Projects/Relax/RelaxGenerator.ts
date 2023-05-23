a5d6e87df54e720b33116c3f171e167b00ff0aed
Matt Nichols <mattnichols206@gmail.com>
2023-05-23 16:11:42 -0700

    Relax: initial vision complete

--- src/Sketches/Projects/Relax/RelaxGenerator.ts
+++ src/Sketches/Projects/Relax/RelaxGenerator.ts
@@ -3,7 +3,7 @@ import type { Path } from 'd3-path';
 
 // Configuration constants
 // todo: make these configurable?
-const pointsPerSegment = 10; // two segments per polygon side
+const pointsPerSegment = 100; // two segments per polygon side
 const polygonSides = 4;
 const generationPercentage = 0.25;
 const rotationPercentage = 0.75;
@@ -17,7 +17,7 @@ const pointRotationOffset = Math.ceil(rotationPercentage * (fullResolution - 1))
 const polygonPointRotationOffset = Math.ceil(polygonRotationPercentage * (fullResolution - 1));
 
 export default class RelaxGenerator {
-    private generatePolygonPath(center: [number, number], radius: number): Path {
+    private generatePolygonPath(center: [number, number], radius: number): [number, number][] {
         // Calculate points around a polygon
         const polygonPoints: [number, number][] = [];
         for (let pointIdx = 0; pointIdx < pointsToGenerate; pointIdx++) {
@@ -44,10 +44,10 @@ export default class RelaxGenerator {
             ];
             polygonPoints.push(point);
         }
-        return CurveUtil.createCardinalSpline(polygonPoints, 0);
+        return polygonPoints;
     }
 
-    private generateCirclePath(center: [number, number], radius: number): Path {
+    private generateCirclePath(center: [number, number], radius: number): [number, number][] {
         // Calculate points around a circle
         const circlePoints: [number, number][] = [];
         for (let pointIdx = 0; pointIdx < pointsToGenerate; pointIdx++) {
@@ -58,23 +58,42 @@ export default class RelaxGenerator {
             ];
             circlePoints.push(point);
         }
-        return CurveUtil.createCardinalSpline(circlePoints, 1);
+        return circlePoints;
     }
 
-    public generate(size: [number, number]): Path[] {
+    public generate(size: [number, number], pathCount = 100): Path[] {
         // Constants for positioning and sizing
         const insetSize = inset * Math.min(size[0], size[1]);
         const topLeft = [insetSize, insetSize];
         const bottomRight = size.map((d) => d - insetSize);
-        const radius = Math.min(size[0], size[1]) / 4;
+        const radius1 = Math.min(size[0], size[1]) / 3;
+        const radius2 = Math.min(size[0], size[1]) / 2;
         const center1: [number, number] = [topLeft[0], bottomRight[1]];
-        const center2: [number, number] = [bottomRight[0] - radius, topLeft[1] + radius];
+        const center2: [number, number] = [bottomRight[0] - radius2, topLeft[1] + radius2];
 
         // Generate the two paths
-        const polygonPath = this.generatePolygonPath(center1, radius);
-        const circlePath = this.generateCirclePath(center2, radius);
+        const polygonPoints = this.generatePolygonPath(center1, radius1);
+        const circlePoints = this.generateCirclePath(center2, radius2);
+        if (!polygonPoints.length || !circlePoints.length)
+            throw 'Guide paths should have the same length';
+
+        // Interpolate between the paths
+        const paths: Path[] = [];
+        for (let pathIdx = 0; pathIdx < pathCount; pathIdx++) {
+            const t = pathIdx / (pathCount - 1);
+            const pathPoints: [number, number][] = [];
+            for (let pointIdx = 0; pointIdx < polygonPoints.length; pointIdx++) {
+                const point: [number, number] = [
+                    polygonPoints[pointIdx][0] * (1 - t) + circlePoints[pointIdx][0] * t,
+                    polygonPoints[pointIdx][1] * (1 - t) + circlePoints[pointIdx][1] * t,
+                ];
+                pathPoints.push(point);
+            }
+            const path = CurveUtil.createCardinalSpline(pathPoints, 0); // effectively a polyline...
+            paths.push(path);
+        }
 
         // Return the two paths
-        return [polygonPath, circlePath];
+        return paths;
     }
 }
